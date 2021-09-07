da20dc888374573871ddacd86afd27e9f1865f00
Crowdbotics <team@crowdbotics.com>
2021-09-07 02:02:20 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "black_heart_30367Identifier",
+  url: "https://black_heart_30367Identifier.botics.co",
+  api: "https://black_heart_30367Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
